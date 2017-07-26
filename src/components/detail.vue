7b1b0723e44eb2f1e471c822eec68059c5cffb76
xygcxy <729815936@qq.com>
2017-07-26 18:57:05 +0800

    playpage

---
+++ src/components/detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="mod_focus">
+        <div class="focus_bg"></div>
+        <div class="focus_title"></div>
+        <div class="focus_gradient"></div>
+    </div>
+    <ul class="song_list"></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+
+</style>
